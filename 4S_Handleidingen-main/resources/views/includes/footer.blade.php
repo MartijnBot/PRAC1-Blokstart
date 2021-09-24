43042ad7d3840910e380d8bee78e44ae2a8e53c4
MartijnBot <d280744@edu.rocwb.nl>
2021-09-24 14:30:44 +0200

    full-project

---
+++ 4S_Handleidingen-main/resources/views/includes/footer.blade.php
@@ -0,0 +1,45 @@
+
+<footer>
+  <div class="full-footer">
+    <div class="footer-start">
+      <p>© Copyright 2021 Avarix</p>
+      <p>John Doe</p>
+      <p>Hoogstraat 123</p>
+    
+    
+    </div>
+    <div class="footer-socials">
+      <i class="fab fa-youtube"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-facebook-square"></i>
+      </div>
+    <div class="footer-links">
+        <a href="#">Over ons</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+</footer>
+
+
+<!-- analytics code -->              
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30506707-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- Einde analytics code -->
+
+<script language="Javascript" type="text/javascript"> 
+ <!-- 
+ if (top.location!= self.location) { 
+  top.location = self.location.href
+ } 
+//--> 
+</script>
